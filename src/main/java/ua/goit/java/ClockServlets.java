718ae8126bc241a7579e9e02fc4ada439ec38b26
kyrychenko <kyrychenko.zakhar@gmail.com>
2017-04-10 16:44:40 +0300

    web service is work

---
+++ src/main/java/ua/goit/java/ClockServlets.java
@@ -0,0 +1,18 @@
+package ua.goit.java;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+public class ClockServlets extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("current time is " + new Date());
+    }
+}
--- src/main/java/й.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Home on 10.04.2017.
- */
-public class й {
-    public static void main(String[] args) {
-        System.out.println("hhhhhhh");
-    }
-}
